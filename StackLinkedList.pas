71049fc8b9ed27052501db9deb72a898878ba675
matthewJamesAbbott <mattbachg@gmail.com>
2022-08-23 20:40:36 +1000

    modified:   StackLinkedList.pas

--- StackLinkedList.pas
+++ StackLinkedList.pas
@@ -24,8 +24,12 @@ type
         procedure addNode(inputData: integer);
         procedure deleteFirstNode();
         procedure deleteLastNode();
+        procedure deleteSpecificNode(nodeNumber: integer);
         function countNodes(): integer;
         function returnSpecificNodesData(nodeNumber: integer): integer;
+        function returnHeadsData(): integer;
+        function returnTailsData(): integer;
+        function returnNodeNumberOfFirstInstanceOfData(inputData: integer): integer;
     end;
 var 
     head : node;
@@ -75,9 +79,28 @@ begin
 
 end;
 
+procedure TStackLinkedList.deleteSpecificNode(nodeNumber: integer);
+var
+    temp1,temp2: node;
+    index: integer;
+begin
+    if nodeNumber = 1 then
+        deleteFirstNode()
+    else
+        begin
+            temp1 := head;
+            for index := 2 to nodeNumber do
+                begin
+                    temp2 := temp1;
+                    temp1 := temp1^.next
+                end;
+            temp2^.next := temp1^.next;
+        end;
+end;
+
 function TStackLinkedList.countNodes(): integer;
 var
-    index, indexAdd: integer;
+    index: integer;
     temp: node;
 begin
     if head = nil then
@@ -89,8 +112,7 @@ begin
             while temp^.next <> nil do
                 begin
                     temp := temp^.next;
-                    indexAdd := index;
-                    index := indexAdd + 1;
+                    inc(index);
                 end;
             countNodes := index;
         end;
@@ -112,5 +134,45 @@ begin
                 end;
         end;
     returnSpecificNodesData := temp^.data;
+end;
+
+function TStackLinkedList.returnHeadsData(): integer;
+begin
+    returnHeadsData := head^.data;
+end;
+
+function TStackLinkedList.returnTailsData(): integer;
+var
+    temp: node;
+begin
+    temp := head;
+    while temp^.next <> nil do
+        begin
+            temp := temp^.next;
+        end;
+    returnTailsData := temp^.data;
+end;
+
+function TStackLinkedList.returnNodeNumberOfFirstInstanceOfData(inputData: integer): integer;
+var
+    temp: node;
+    index: integer;
+begin
+    temp := head;
+    if temp^.data = inputData then
+        returnNodeNumberOfFirstInstanceOfData := 1
+    else
+        begin
+            index := 1;
+            while temp^.next <> nil do
+                begin
+                    inc(index);
+                    temp := temp^.next;
+                    if temp^.data = inputData then
+                        begin
+                            returnNodeNumberOfFirstInstanceOfData := index; 
+                        end;
+                end;
+        end;
 end;
 end.
