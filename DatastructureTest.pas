f509c813a532ef845d5bfc59441f9595e006e026
matthewJamesAbbott <mattbachg@gmail.com>
2022-08-25 11:02:14 +1000

    modified:   DatastructureTest.pas

--- DatastructureTest.pas
+++ DatastructureTest.pas
@@ -117,36 +117,100 @@ begin
     stackDoubleLinkedListVar.create();
     writeln('created list object');
     stackDoubleLinkedListVar.insertFirst(3);
-    writeln('inserted the number 3 into list');
+    writeln('inserted the number 3 into First position of list');
     stackDoubleLinkedListVar.insertFirst(2);
-    writeln('inserted the number 2 into list');
+    writeln('inserted the number 2 into First position of list');
     stackDoubleLinkedListVar.insertFirst(7);
-    writeln('inserted the number 7 into list');
+    writeln('inserted the number 7 into First position of list');
     stackDoubleLinkedListVar.insertFirst(5);
-    writeln('inserted the number 5 into list');
+    writeln('inserted the number 5 into First position of list');
     stackDoubleLinkedListVar.insertFirst(3);
-    writeln('inserted the number 3 into list');
+    writeln('inserted the number 3 into First position of list');
     stackDoubleLinkedListVar.insertFirst(5);
-    writeln('inserted the number 5 into list');
+    writeln('inserted the number 5 into First position of list');
     stackDoubleLinkedListVar.insertFirst(1);
-    writeln('inserted the number 1 into list');
+    writeln('inserted the number 1 into First position of list');
     stackDoubleLinkedListVar.insertFirst(3);
-    writeln('inserted the number 3 into list');
+    writeln('inserted the number 3 into First position of list');
     stackDoubleLinkedListVar.insertFirst(9);
-    writeln('inserted the number 9 into list');
+    writeln('inserted the number 9 into First position of list');
     stackDoubleLinkedListVar.insertFirst(4);
-    writeln('inserted the number 4 into list');
+    writeln('inserted the number 4 into First position of list');
     stackDoubleLinkedListVar.insertFirst(8);
-    writeln('inserted the number 8 into list');
+    writeln('inserted the number 8 into First position of list');
     stackDoubleLinkedListVar.insertFirst(3);
-    writeln('inserted the number 3 into list');
+    writeln('inserted the number 3 into First position of list');
     stackDoubleLinkedListVar.insertFirst(1);
-    writeln('inserted the number 1 into list');
+    writeln('inserted the number 1 into First position of list');
     writeln('there are ', stackDoubleLinkedListVar.countNodes(), ' nodes in list');
     for index := 1 to stackDoubleLinkedListVar.countNodes() do
          begin
              writeln('extracting data from node ', index , ' : ' , stackDoubleLinkedListVar.returnSpecificNodesData(index));
          end;
+
+    stackDoubleLinkedListVar.insertLast(3);
+    writeln('inserted the number 3 into last position of the list');
+    stackDoubleLinkedListVar.insertLast(6);
+    writeln('inserted the number 6 into last position of the list');
+    stackDoubleLinkedListVar.insertLast(8);
+    writeln('inserted the number 8 into last position of the list');
+    stackDoubleLinkedListVar.insertLast(9);
+    writeln('inserted the number 9 into last position of the list');
+    stackDoubleLinkedListVar.insertLast(6);
+    writeln('inserted the number 6 into last position of the list');
+    stackDoubleLinkedListVar.insertLast(0);
+    writeln('inserted the number 0 into last position of the list');
+    stackDoubleLinkedListVar.insertLast(2);
+    writeln('inserted the number 2 into last position of the list');
+    stackDoubleLinkedListVar.insertLast(7);
+    writeln('inserted the number 7 into last position of the list');
+    stackDoubleLinkedListVar.insertLast(5);
+    writeln('inserted the number 5 into last position of the list');
+    stackDoubleLinkedListVar.insertLast(3);
+    writeln('inserted the number 3 into last position of the list');
+    stackDoubleLinkedListVar.insertLast(3);
+    writeln('inserted the number 3 into last position of the list');
+    stackDoubleLinkedListVar.insertLast(3);
+    writeln('inserted the number 3 into last position of the list');
+    stackDoubleLinkedListVar.insertLast(8);
+    writeln('inserted the number 8 into last position of the list');
+    stackDoubleLinkedListVar.insertLast(6);
+    writeln('inserted the number 6 into last position of the list');
+    stackDoubleLinkedListVar.insertLast(4);
+    writeln('inserted the number 4 into last position of the list');
+    stackDoubleLinkedListVar.insertLast(0);
+    writeln('inserted the number 0 into last position of the list');
+    stackDoubleLinkedListVar.insertLast(4);
+    writeln('inserted the number 4 into last position of the list');
+    stackDoubleLinkedListVar.insertLast(1);
+    writeln('inserted the number 1 into last position of the list');
+    writeln('there are ', stackDoubleLinkedListVar.countNodes(), ' nodes in list');
+    for index := 1 to stackDoubleLinkedListVar.countNodes() do
+          begin
+              writeln('extracting data from node ', index , ' : ' , stackDoubleLinkedListVar.returnSpecificNodesData(index));
+          end;
+    stackDoubleLinkedListVar.deleteFirst();
+    writeln('the first node has been deleted');
+    writeln('there are ', stackDoubleLinkedListVar.countNodes(), ' nodes in list');
+    for index := 1 to stackDoubleLinkedListVar.countNodes() do
+           begin
+               writeln('extracting data from node ', index , ' : ' , stackDoubleLinkedListVar.returnSpecificNodesData(index));
+           end;
+    stackDoubleLinkedListVar.insertAfter(8,8);
+    writeln('inserted 8 after the first instance of 8');
+    for index := 1 to stackDoubleLinkedListVar.countNodes() do
+           begin
+               writeln('extracting data from node ', index , ' : ' , stackDoubleLinkedListVar.returnSpecificNodesData(index));
+           end;
+    writeln('node 23 contains : ', stackDoubleLinkedListVar.returnSpecificNodesData(23));
+    stackDoubleLinkedListVar.deleteLast();
+    writeln('the last node of the list has been deleted');
+    writeln('there are ', stackDoubleLinkedListVar.countNodes(), ' nodes in list');
+    for index := 1 to stackDoubleLinkedListVar.countNodes() do
+           begin
+                writeln('extracting data from node ', index , ' : ' , stackDoubleLinkedListVar.returnSpecificNodesData(index));
+           end;
+
 
     { -----END PROCSS TESTS FOR StackDoubleLinkedList.pas----- }
 
